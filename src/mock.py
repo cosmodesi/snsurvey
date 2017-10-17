5517e6f9620080e7bae4d0dc702b514d98c79976
Alex Kim <akim@201.51.243.131.in-addr.dhcp.lbl.gov>
2017-10-17 13:46:19 -0700

    combine tier 1 and 2

--- src/mock.py
+++ src/mock.py
@@ -19,15 +19,26 @@ zedge = lowerlimit + numpy.arange(len(cumcount))*binsize
 
 zbins = numpy.arange(0,0.5,0.02)
 ans = []
+ans2 = []
 for i in xrange(len(zbins)-1):
     w = numpy.logical_and(f['Data']['z_cos'].value > zbins[i], f['Data']['z_cos'].value < zbins[i+1])
-    w = numpy.logical_and(w,f['Data']['app_mag'].value <19.5)
-    lim  = abs_mag[w].max()
+    w1 = numpy.logical_and(w,f['Data']['app_mag'].value <19.5)
+    lim  = abs_mag[w1].max()
     ans.append(numpy.interp(lim, zedge,cumcount))
+    w1 = numpy.logical_and(w,f['Data']['app_mag'].value <20.)
+    lim  = abs_mag[w1].max()
+    ans2.append(numpy.interp(lim, zedge,cumcount))
+
 
 ans=numpy.array(ans)
+ans2=numpy.array(ans2)
+diff = ans2-ans
 
-plt.plot(zbins[1:],ans)
+ans = ans*.97*.92
+ans2 = ans + diff*.92*.77
+# plt.plot(zbins[1:],ans,label=r'$r< 19.5$')
+plt.plot(zbins[1:],ans2)
+# plt.legend()
 plt.xlabel('redshift')
-plt.ylabel(r'fraction of SNe Ia in galaxies brighter than $r=19.5$')
+plt.ylabel(r'fraction of SNe Ia in galaxies with BGS redshift')
 plt.savefig('bg_frac.pdf')
